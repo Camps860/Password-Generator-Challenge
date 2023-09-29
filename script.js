09f2389da164d86bc24908bcba260c84fa7214d9
Sean C <139496512+Camps860@users.noreply.github.com>
2023-09-28 19:25:46 -0500

    added js elements

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
 // Assignment code here
 var characterLength = 8;
-var choiceArr = [];
+var choiceArr = ['!', '@', '#', '$', '%', '&', '*'];
+
 
 var specialCharArr = ['!', '@', '#', '$', '%', '&', '*'];
 var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 
-// Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
 
 
 // Write password to the #password input
-function writePassword() {
-  var correctPrompts = getPrompts();
-  var passwordText = document.querySelector("password");
+var passwordText = document.querySelector("#password");
+  passwordText.value = password;  
 
-  if (correctPrompts) {
-    var password = generatePassword();
-    passwordText.value = password;
-  } else {
-    passwordTest.value = "newPassword";
-  }
+
+var password = generatePassword();
+
+
+
+function writePassword() {
+ 
+  
 }
 function generatePassword() {
-  var password = "password";
+  var password = "";
   for (var i = 0; i < characterLength; i++) {
     var randomIndex = Math.floor(Math.random() * choiceArr.length);
-    password = password + choice[randomIndex];
+    password = password + choiceArr[randomIndex];
   }
   return password;
 }
-function getPrompts() {
-  choiceArr = [];
-  characterLength = parseInt(prompt("How maany characters do you want in your password? (8-128 charcters"));
-}
-if (!(characterLength) || characterLength < 8 || characterLength > 128) {
-  alert("Character length has to be a number, 8-128 digits. Please try again.")
-  
-}
+
+
 
 if (confirm("Would you like lowercase letters in your password?")) {
   choiceArr = choiceArr.concat(lowerCaseArr);
